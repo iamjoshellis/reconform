6795b644050721ca5a6ec46e50ee2ac9677e5282
Josh Ellis <iamjoshellis@gmail.com>
2018-03-09 11:49:53 +1100

    Named exports 🏎

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import withFields from "./hocs/withFields";
 import withSubmit from "./hocs/withSubmit";
 import getFieldKeyValues from "./utils/getFieldKeyValues";
 
+export { default as withFields } from "./hocs/withFields";
+export { default as withSubmit } from "./hocs/withSubmit";
+export { default as getFieldKeyValues } from "./utils/getFieldKeyValues";
+
 export default {
   withFields,
   withSubmit,
